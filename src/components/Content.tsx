d8defccdab2b77fb3c88169fa2c6ce6b3b77b551
Nicolas <nicolas@galaxpay.com.br>
2021-08-23 18:45:28 -0300

    Refatorado para cada componente ter sua responsabilidade.

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,38 @@
-export function Content() {
-  // Complete aqui
+import { MovieCard } from "./MovieCard";
+
+
+interface Movie {
+  imdbID: string;
+  Title: string;
+  Poster: string;
+  Ratings: Array<{
+    Value: string;
+  }>;
+  Runtime: string;
+}
+
+interface Genre{
+  title: string;
+}
+interface ContentProps {
+  selectedGenre: Genre;
+  movies: Movie[];
+}
+
+export function Content(props: ContentProps) {
+  return (
+    <div className="container">
+        <header>
+          <span className="category">Categoria:<span> {props.selectedGenre.title}</span></span>
+        </header>
+
+        <main>
+          <div className="movies-list">
+            {props.movies.map(movie => (
+              <MovieCard key ={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+            ))}
+          </div>
+        </main>
+      </div>
+  );
 }
